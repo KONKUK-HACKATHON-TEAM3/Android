46246fc656b50ab6dd0f46c3f9d1e2e2142c0c26
MinseoSONG <mins041600@naver.com>
2025-08-26 17:59:35 +0900

    [fix] 충돌해결

--- app/src/main/java/com/konkuk/hackathon_team3/presentation/main/MainNavHost.kt
+++ app/src/main/java/com/konkuk/hackathon_team3/presentation/main/MainNavHost.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.konkuk.hackathon_team3.presentation.minseo.RankingRoute
+import com.konkuk.hackathon_team3.presentation.minseo.ranking.RankingRoute
 import com.konkuk.hackathon_team3.presentation.minseok.GasWritingRoute
 
 @Composable
