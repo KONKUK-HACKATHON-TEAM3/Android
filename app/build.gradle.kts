80108cc6cf4be9f9c2445ddda28d66cebd998c77
t1nm1ksun <minsuk07672@naver.com>
2025-08-27 23:59:50 +0900

    [feat] 업로드 뷰 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.navigation)
+    implementation(libs.androidx.animation.core)
 
     // Test Libraries
     testImplementation(libs.junit)
